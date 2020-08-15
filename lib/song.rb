5badcf0457a650d51063df3d83fbeaa268a4ea2a
Kevin Britt <kbritt510@gmail.com>
2020-08-15 04:37:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,10 @@ def self.all
   @@all
 end
 
-def self.new_by_filename(file_name)
-  
+def self.new_by_filename(filename)
+  song = self.new
+  song.title = filename.split(" - ")[1]
+  song
 end
 
 
