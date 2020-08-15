c0f45175156a1c6abb8c623cbdb1b9e9830af8f5
Kevin Britt <kbritt510@gmail.com>
2020-08-15 04:59:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,8 +27,8 @@ end
 #   song.artist = song_artist
 # end
 
-def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
+def self.new_by_filename(filename)
+    song_info = filename.chomp(".mp3").split(" - ")
     song = Song.new(song_info[1])
     song.artist_name = song_info[0]
     song
