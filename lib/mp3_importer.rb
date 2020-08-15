9836fc175b947d1d812f3551d2e3a25ad0b87917
Kevin Britt <kbritt510@gmail.com>
2020-08-15 04:37:58 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,23 +10,10 @@ def files
       @files = Dir.glob("#{@path}/*.mp3").collect{ |file| file.gsub("#{@path}/", "") }
     end
 
-
-
-
-
 end
 
 
 
-
-class Song
-  attr_accessor :title
- 
-  def self.new_by_filename(filename)
-    song = self.new
-    song.title = filename.split(" - ")[1]
-    song
-  end
  
 end
  
